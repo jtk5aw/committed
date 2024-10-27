6f9129270c9e6fe56525ded8e949540ea7697380
Jackson Kennedy <jacksontkennedy99@gmail.com>
2024-10-27 13:40:39 -0400

    Play hono app

--- sst.config.ts
+++ sst.config.ts
@@ -1,12 +1,25 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
-  app(input) {
-    return {
-      name: "committed",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      home: "cloudflare",
-    };
-  },
-  async run() {},
+    app(input) {
+        return {
+            name: "committed",
+            removal: input?.stage === "production" ? "retain" : "remove",
+            home: "cloudflare",
+        };
+    },
+    async run() {
+        const bucket = new sst.cloudflare.Bucket("MyBucket");
+
+        const hono = new sst.cloudflare.Worker("Hono", {
+            url: true,
+            handler: "index.ts",
+            link: [bucket],
+        });
+
+
+        return {
+            api: hono.url,
+        };
+    }
 });
