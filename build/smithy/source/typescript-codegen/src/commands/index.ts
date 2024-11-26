cbf58a30114bf77d2d76b308fd7b9f962ec2723f
Jackson Kennedy <jacksontkennedy99@gmail.com>
2024-11-26 11:46:31 -0500

    Delete build dir

--- build/smithy/source/typescript-codegen/src/commands/index.ts
+++
@@ -1,7 +0,0 @@
-// smithy-typescript generated code
-export * from "./CreateCityCommand";
-export * from "./DeleteCityCommand";
-export * from "./GetCityCommand";
-export * from "./GetCurrentTimeCommand";
-export * from "./GetForecastCommand";
-export * from "./ListCitiesCommand";
